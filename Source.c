cd5f62e657f769122d5c4a13d916b8ef2f0272a5
jasonluu12 <jasonluu121@gmail.com>
2016-09-13 13:29:03 -0400

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("|------------------------------------|\n");
+	printf("|she sells sea shells by the seashore|\n");
+	printf("|she sells sea shells by the seashore|\n");
+	printf("|she sells sea shells by the seashore|\n");
+	printf("|------------------------------------|\n");
+	return(0);
+}
